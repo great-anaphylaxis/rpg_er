2644bab60d49173dffad118c11f48e83b2bb29e0
Jhareign <74128037+Jhareign@users.noreply.github.com>
2022-08-26 20:19:52 +0800

    a basic animation system

--- rpg'er.js
+++ rpg'er.js
@@ -4,7 +4,10 @@ var canvas = document.querySelector("canvas").getContext("2d");
 var viewport = document.querySelector("#viewport");
 
 var images = {
-    player: init_image("images/player.png"),
+    player_idle_up: init_image("images/player_idle_up.png"),
+    player_idle_down: init_image("images/player_idle_down.png"),
+    player_idle_left: init_image("images/player_idle_left.png"),
+    player_idle_right: init_image("images/player_idle_right.png"),
     metal_tile: init_image("images/metal_tile.png"),
     oil_tile: init_image("images/oil_tile.png")
 };
@@ -21,18 +24,33 @@ var player = {
     height: 32,
     speed: 5
 };
+var animations = {
+    player: {
+        speed: 0,
+        frames: [images.player_idle_down],
+        currentFrame: 0
+    }
+};
 
 var map = [[]];
+var ticks = 1;
 
-function init_map() {
-    for (let x = 0; x < 100; x++) {
-        map[x] = [];
-        for (let y = 0; y < 100; y++) {
-            map[x][y] = {tile: {name: 'metal_tile', isWalkable: true}};
+function init_map(premap = undefined) {
+    if (premap == undefined) {
+        for (let x = 0; x < 100; x++) {
+            map[x] = [];
+            for (let y = 0; y < 100; y++) {
+                map[x][y] = {tile: {name: 'metal_tile', isWalkable: true}};
+            }
         }
     }
 
+    else {
+        map = premap;
+    }
+
     map[5][5] = {tile: {name: 'oil_tile', isWalkable: false}};
+    map[99][99] = {tile: {name: 'oil_tile', isWalkable: false}};
 }
 
 function init_image(path) {
@@ -89,8 +107,19 @@ addEventListener("keyup", function(e) {
 setTimeout(function() {
     setInterval(function() {
         canvas.clearRect(0, 0, canvas.canvas.width, canvas.canvas.height);
+
+        ticks = ticks + 1;
+        if (ticks > 2000000000) {
+            ticks = 1;
+        }
+
         if (keys.arrowUp == true) {
+            animations.player.frames[0] = images.player_idle_up;
             for (let i = 0; i < player.speed; i++) {
+                if (player.y <= 0) {
+                    break;
+                }
+
                 let currentBlockX = clamp(parseInt(player.x / 32), 0, 99);
                 let currentBlockY = clamp(parseInt(player.y / 32), 0, 99);
 
@@ -131,7 +160,12 @@ setTimeout(function() {
             }
         }
         if (keys.arrowDown == true) {
+            animations.player.frames[0] = images.player_idle_down;
             for (let i = 0; i < player.speed; i++) {
+                if (player.y >= 3200) {
+                    break;
+                }
+
                 let currentBlockX = clamp(parseInt(player.x / 32), 0, 99);
                 let currentBlockY = clamp(parseInt(player.y / 32), 0, 99);
 
@@ -172,7 +206,12 @@ setTimeout(function() {
             }
         }
         if (keys.arrowLeft == true) {
+            animations.player.frames[0] = images.player_idle_left;
             for (let i = 0; i < player.speed; i++) {
+                if (player.x <= 0) {
+                    break;
+                }
+
                 let currentBlockX = clamp(parseInt(player.x / 32), 0, 99);
                 let currentBlockY = clamp(parseInt(player.y / 32), 0, 99);
 
@@ -213,7 +252,12 @@ setTimeout(function() {
             }
         }
         if (keys.arrowRight == true) {
+            animations.player.frames[0] = images.player_idle_right;
             for (let i = 0; i < player.speed; i++) {
+                if (player.x >= 3200) {
+                    break;
+                }
+
                 let currentBlockX = clamp(parseInt(player.x / 32), 0, 99);
                 let currentBlockY = clamp(parseInt(player.y / 32), 0, 99);
 
@@ -271,7 +315,22 @@ setTimeout(function() {
                 }
             }
         }
+
+        for (let i in animations) {
+            let anim = animations[i];
+
+            if (anim.speed == null) {
+
+            }
+
+            else if (ticks % anim.speed == 0) {
+                anim.currentFrame += 1;
+                if (anim.currentFrame >= anim.frames.length) {
+                    anim.currentFrame = 0;
+                }
+            }
+        }
     
-        canvas.drawImage(images.player, player.x, player.y, player.width, player.height);
+        canvas.drawImage(animations.player.frames[animations.player.currentFrame], player.x, player.y, player.width, player.height);
     }, 20);
 }, 20);
